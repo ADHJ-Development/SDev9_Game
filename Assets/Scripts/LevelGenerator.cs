a38311bf181530baa680af39c10e15768b2eeadf
Gabriel Hager <gabriel.a.hager@gmail.com>
2021-03-06 13:50:43 -0800

    Generate Random Enemies
    Completed Random Generation Script

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -5,12 +5,39 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
-   // public static Random rand = new Random();
-    //public static int randXPos = rand.Next(0, 4);
     public Transform Enemy_1;
+    public Transform Enemy_2;
+    public Transform Enemy_3;
+
    private void Awake()
     {
-        Instantiate(Enemy_1, new Vector3(Random.Range(-6,8), -0.5f), Quaternion.identity);
+        for(int i = 0; i < Random.Range(3,8); i++)
+        {
+            generateEnemy(RandomEnemy(Random.Range(1,4)));
+           
+        }
     }
 
+    private void generateEnemy(Transform enemyTransform)
+    {
+        Instantiate(enemyTransform, new Vector3(Random.Range(-6, 8), -0.5f), Quaternion.identity);
+    }
+
+    public Transform RandomEnemy(int enemyNumber)
+    {
+        switch(enemyNumber)
+        {
+            case 1:
+                return Enemy_1;
+                break;
+            case 2:
+                return Enemy_2;
+                break;
+            case 3:
+                return Enemy_3;
+                break;
+            default:
+                return Enemy_1;     
+        }    
+    }
 }

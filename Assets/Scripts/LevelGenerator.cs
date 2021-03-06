2bd51ad9eefc85017361e3a8ba181ebee1afc4b7
Gabriel Hager <gabriel.a.hager@gmail.com>
2021-03-06 12:52:27 -0800

    Added Level Generation Template

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+   // public static Random rand = new Random();
+    //public static int randXPos = rand.Next(0, 4);
+    public Transform Enemy_1;
+   private void Awake()
+    {
+        Instantiate(Enemy_1, new Vector3(2, -0.5f), Quaternion.identity);
+    }
+
+}
